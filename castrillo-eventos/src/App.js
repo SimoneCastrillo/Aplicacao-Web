46f4a19c92e96ddf76544fed84262d4a444822b9
Igor Anthony <igor.maciel@sptech.school>
2024-08-28 20:52:11 -0300

    commitando

--- castrillo-eventos/src/App.js
+++ castrillo-eventos/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/" element={<h1>Homeaaaa</h1>} />
         <Route path="*" element={<h1>Not Found</h1>} />
       </Routes>
     </BrowserRouter>
