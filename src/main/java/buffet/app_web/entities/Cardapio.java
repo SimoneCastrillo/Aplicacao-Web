ff05830a5a22816aaad5fdae2ac2ba1c670a3f53
Igor Anthony <igor.maciel@sptech.school>
2024-08-28 20:23:29 -0300

    Alterando o diretorio

--- src/main/java/buffet/app_web/entities/Cardapio.java
+++
@@ -1,9 +0,0 @@
-package buffet.app_web.entities;
-
-import jakarta.persistence.Entity;
-
-import java.util.List;
-
-public class Cardapio {
-    private List<ItemCardapio> itens;
-}
--- src/main/java/buffet/app_web/repositories/ItemCardapioRepository.java
+++
@@ -1,7 +0,0 @@
-package buffet.app_web.repositories;
-
-import buffet.app_web.entities.ItemCardapio;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemCardapioRepository extends JpaRepository<ItemCardapio, Integer> {
-}
