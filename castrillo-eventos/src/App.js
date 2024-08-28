551206f98bddabbf075fb74dc55a8270a25abd31
Igor Anthony <igor.maciel@sptech.school>
2024-08-28 20:46:09 -0300

    Criação do projeto em react

---
+++ castrillo-eventos/src/App.js
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="*" element={<h1>Not Found</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
