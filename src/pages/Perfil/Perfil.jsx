a6c21fd66f1c099f5cc1aa17aa9cfd87b5b5cf2a
Robson Rioki Nakama <robson.nakama@sptech.school>
2024-11-13 20:08:29 -0300

    correção

--- src/pages/Perfil/Perfil.jsx
+++ src/pages/Perfil/Perfil.jsx
@@ -1,29 +1,53 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import HeaderPerfil from '../../components/headerPerfil/HeaderPerfil'
 import styles from './Perfil.module.css'
 import Reservas from '../../components/Reservas/Reservas';
 import MeusDados from '../../components/MeusDados/MeusDados';
 import ModalFoto from '../../components/MeusDados/modalFoto/ModalFoto';
+import ModalCancelarEditar from '../../components/MeusDados/modalCancelarEditar/ModalCancelarEditar';
+import Calendario from '../../components/Calendario/Calendario';
+import ModalCancelarReserva from '../../components/Reservas/ModalCancelarReserva/ModalCancelarReserva';
 import DecoracoesEdicao from '../../components/DecoracoesEdicao/DecoracoesEdicao';
 
 const Perfil = () => {
-    const [modalFoto, setModalFoto] = useState(false);
-    const [img, setImg] = useState('');
-    const [nomeUser, setNomeUser] = useState('');
-    const [componenteParaExibir, setComponenteParaExibir] = useState('meus-dados');
-    const handleEscolherComponente = (componente) => {
+  const [modalFoto, setModalFoto] = useState(false);
+  const [img, setImg] = useState('');
+
+  const [modalCancelarEdit, setModalCancelarEdit] = useState(false);
+  const [descartarAlteracoes, setDescartarAlteracoes] = useState(false);
+
+  const [modalCancelarReserva, setModalCancelarReserva] = useState(false);
+  const [cancelarReserva, setCancelarReserva] = useState(false);
+
+  const [nomeUser, setNomeUser] = useState('');
+  const [componenteParaExibir, setComponenteParaExibir] = useState('meus-dados');
+  const [userRole, setUserRole] = useState('normal');
+
+  const handleEscolherComponente = (componente) => {
     setComponenteParaExibir(componente);
-    }
+  }
+
   return (
     <div className={styles.container}>
-      {modalFoto && <ModalFoto onSetImg={setImg} onFoto={img} onCloseModalFoto={() => setModalFoto(false)}/>}
-      <HeaderPerfil componenteAtivo={componenteParaExibir} onNomeUser={nomeUser} onEscolherComponente={handleEscolherComponente} />
+      {modalFoto && <ModalFoto onSetImg={setImg} onFoto={img} onCloseModalFoto={() => setModalFoto(false)} />}
+      {modalCancelarEdit && <ModalCancelarEditar onCloseModalEdit={() => setModalCancelarEdit(false)} onSetDescartarAlteracoes={setDescartarAlteracoes} />}
+      {modalCancelarReserva && <ModalCancelarReserva onCloseModalReserva={() => setModalCancelarReserva(false)} onSetCancelarReserva={setCancelarReserva} />}
+      <HeaderPerfil componenteAtivo={componenteParaExibir} onNomeUser={nomeUser} onEscolherComponente={handleEscolherComponente} onUserRole={userRole} />
       <div className={styles.box}>
-        {componenteParaExibir === 'minhas-reservas' && <Reservas/>}
-        {componenteParaExibir === 'decoracao' && <DecoracoesEdicao/>}
-        {componenteParaExibir === 'meus-dados' && <MeusDados onImg={img} onSetImg={setImg} onSetNomeUser={setNomeUser} onOpenModalFoto={()=> setModalFoto(true)} />}
+        {componenteParaExibir === 'minhas-reservas' && <Reservas onSetCancelarReserva={() => setCancelarReserva(false)} onCancelarReserva={cancelarReserva} openModalCacelarReserva={() => setModalCancelarReserva(true)} />}
+        {componenteParaExibir === 'meus-dados' && <MeusDados onImg={img}
+          onSetImg={setImg}
+          onSetNomeUser={setNomeUser}
+          onSetUserRole={setUserRole}
+          onOpenModalCancelarEditar={() => setModalCancelarEdit(true)} onOpenModalFoto={() => setModalFoto(true)}
+          onDescartarAlteracoes={descartarAlteracoes}
+        />}
+        {componenteParaExibir === 'decoracao' && <DecoracoesEdicao />}
+
+        {componenteParaExibir === 'calendario' && <Calendario />}
+        {componenteParaExibir === 'reservas' && <Reservas onCancelarReserva={cancelarReserva} onSetCancelarReserva={() => setCancelarReserva(false)} openModalCacelarReserva={() => setModalCancelarReserva(true)} />}
       </div>
-      
+
     </div>
   )
 }
